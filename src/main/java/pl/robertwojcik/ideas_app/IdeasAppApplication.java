087b5f131759f0bff6b6cc5516124f43efad57e7
Robert Wójcik <robert.wojcik.programista@gmail.com>
2023-02-23 21:09:19 +0100

    init commit

---
+++ src/main/java/pl/robertwojcik/ideas_app/IdeasAppApplication.java
@@ -0,0 +1,13 @@
+package pl.robertwojcik.ideas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeasAppApplication.class, args);
+	}
+
+}
