bf78ffca1db12ae159e847f01a405112acfd370e
Robert Wójcik <robert.wojcik.programista@gmail.com>
2023-02-23 22:10:10 +0100

    feature/added QuestionsController and QuestionsService, first endpoint

---
+++ src/main/java/pl/robertwojcik/ideas_app/IdeasConfiguration.java
@@ -0,0 +1,18 @@
+package pl.robertwojcik.ideas_app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "ideas")
+public class IdeasConfiguration {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/robertwojcik/ideas_app/Question.java
@@ -0,0 +1,28 @@
+package pl.robertwojcik.ideas_app;
+
+public class Question {
+
+    private String name;
+
+    public Question() {
+    }
+
+    public Question(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
